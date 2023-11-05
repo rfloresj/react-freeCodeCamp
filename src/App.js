25837d8d1a1c5dab1da925b1d23f8361078c6bd2
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-05 13:36:23 -0600

    props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
     <div className="App">
       <div className="principal-container">
         <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        <Testimony />
+        <Testimony
+          name="Emma Bostian"
+          country="Sweden"
+          image=""
+          charge="Software Engineer"
+          company="Spotify"
+          testimony="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+        />
       </div>
     </div>
   );
--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../stylesheets/Testimony.css";
 
-function Testimony() {
+function Testimony(props) {
   return (
     <div className="testimony-container">
       <img
@@ -10,15 +10,13 @@ function Testimony() {
         alt="Emma"
       />
       <div className="testimony-text-container">
-        <p className="testimony-name">Emma Bostian in Sweden</p>
-        <p className="charge-name">Software Engineer at Spotify</p>
-        <p className="testimony-text">
-          "I've always struggled with learning JavaScript. I've taken many
-          courses but freeCodeCamp's course was the one which stuck. Studying
-          JavaScript as well as data structures and algorithms on freeCodeCamp
-          gave me the skills and confidence I needed to land my dream job as a
-          software engineer at Spotify."
+        <p className="testimony-name">
+          {props.name} in {props.country}
         </p>
+        <p className="charge-name">
+          {props.charge} at {props.company}
+        </p>
+        <p className="testimony-text">"{props.testimony}"</p>
       </div>
     </div>
   );
