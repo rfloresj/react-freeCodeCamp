9c1a77d15948a9975ec9f882472e7b36389daad1
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-04 19:34:31 -0600

    Init

---
+++ src/components/Testimony.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Testimony() {
+  return (
+    <div className="testimony-container">
+      <img
+        className="testimony-img"
+        src={require("../img/testimony-emma.png")}
+        alt="Emma"
+      />
+      <div className="testimony-text-container">
+        <p className="testimony-name">Emma Bostian in Sweden</p>
+        <p className="charge-name">Software Engineer at Spotify</p>
+        <p className="testimony-text">
+          "I've always struggled with learning JavaScript. I've taken many
+          courses but freeCodeCamp's course was the one which stuck. Studying
+          JavaScript as well as data structures and algorithms on freeCodeCamp
+          gave me the skills and confidence I needed to land my dream job as a
+          software engineer at Spotify."
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimony;
