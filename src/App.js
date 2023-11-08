ffbd4112ef241984648fb1e42696518587809a4b
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-07 20:35:49 -0600

    create-Data.js-to-store-data

--- src/App.js
+++ src/App.js
@@ -1,54 +1,6 @@
+import { DATA_INFO } from "./Data";
 import "./App.css";
 import Testimony from "./components/Testimony";
-
-const DATA_INFO = [
-  {
-    name: "Shawn Wang",
-    country: "Singapore",
-    image: "shawn",
-    charge: "Software Engineer ",
-    company: "Amazon",
-    testimony: (
-      <>
-        It's scary to change careers. I only gained confidence that I could code
-        by working through the hundreds of hours of free lessons on
-        freeCodeCamp. Within a year I had a six-figure job as a Engineer.
-        <strong>freeCodeCamp changed my life </strong>.
-      </>
-    ),
-  },
-  {
-    name: "Sarah Chima",
-    country: "Nigeria",
-    image: "sarah",
-    charge: "Software Engineer",
-    company: "ChatDesk",
-    testimony: (
-      <>
-        <strong>freeCodeCamp was the gateway to my career</strong> as a software
-        developer. The well-structured curriculum took my coding knowledge from
-        a total beginner level to a very confident level. It was everything I
-        needed to land my first dev job at an amazing company.
-      </>
-    ),
-  },
-  {
-    name: "Emma Bostian",
-    country: "Sweden",
-    image: "emma",
-    charge: "Software Engineer",
-    company: "Spotify",
-    testimony: (
-      <>
-        I've always struggled with learning JavaScript. I've taken many courses
-        but freeCodeCamp's course was the one which stuck. Studying JavaScript
-        as well as data structures and algorithms on{" "}
-        <strong>freeCodeCamp gave me the skills</strong> and confidence I needed
-        to land my dream job as a software engineer at Spotify.
-      </>
-    ),
-  },
-];
 
 function App() {
   return (
