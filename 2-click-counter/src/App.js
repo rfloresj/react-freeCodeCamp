a6d34cbb915a547c823a21574b9bf289e8d2ecee
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-09 08:26:57 -0600

    freeCodeCamp-logo

--- 2-click-counter/src/App.js
+++ 2-click-counter/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import freeCodeCampLogo from "./images/freecodecamp-logo.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-container">
+        <img
+          className="freecodecamp-logo"
+          src={freeCodeCampLogo}
+          alt="logo-freeCodeCamp"
+        />
+        <div className="principal-container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
