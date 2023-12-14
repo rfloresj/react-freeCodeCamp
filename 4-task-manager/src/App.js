599569c27ec125c78fd8e6d63d52eef7bdc3d1ed
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-14 12:18:24 -0600

    logo-container

--- 4-task-manager/src/App.js
+++ 4-task-manager/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import freeCodeCampLogo from "./imgs/freecodecamp-logo.png";
 
 function App() {
-  return <div className="App">React</div>;
+  return (
+    <div className="task-manager">
+      <div className="freecodecamp-logo-container">
+        <img
+          src={freeCodeCampLogo}
+          alt="freecodecamp-logo"
+          className="freecodecamp-logo"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
