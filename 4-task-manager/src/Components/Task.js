535c1877a0b072dbb0d91101732be8cb33aef371
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-18 11:37:00 -0600

    React-icons

--- 4-task-manager/src/Components/Task.js
+++ 4-task-manager/src/Components/Task.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Task.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const Task = ({ text }) => {
   return (
     <div className="task-container">
       <div className="task-text">{text}</div>
-      <div className="task-icon">Delete</div>
+      <div className="task-icon">
+        <AiOutlineCloseCircle />
+      </div>
     </div>
   );
 };
