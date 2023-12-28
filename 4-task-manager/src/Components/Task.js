d2eb4cb29453c7a20d782f3d834c386eaec3768f
rflores <104962825+rfloresj@users.noreply.github.com>
2023-12-28 11:39:16 -0600

    show-tasks-list

--- 4-task-manager/src/Components/Task.js
+++ 4-task-manager/src/Components/Task.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./Task.css";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-const Task = ({ text, completed }) => {
+const Task = ({ id, text, completed, completeTask, deleteTask }) => {
   return (
     <div className={completed ? "task-container completed" : "task-container"}>
-      <div className="task-text">{text}</div>
+      <div className="task-text" onClick={completeTask}>
+        {text}
+      </div>
       <div className="task-container-icons">
         <AiOutlineCloseCircle className="task-icon" />
       </div>
