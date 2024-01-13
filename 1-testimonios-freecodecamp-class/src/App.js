086548ac211f1f668bdad2f3e3efbe543ea787d7
rflores <104962825+rfloresj@users.noreply.github.com>
2024-01-13 09:43:19 -0600

    App.js-class-component

--- 1-testimonios-freecodecamp-class/src/App.js
+++ 1-testimonios-freecodecamp-class/src/App.js
@@ -1,18 +1,21 @@
+import React from "react";
 import { DATA_INFO } from "./Data";
 import "./App.css";
 import Testimony from "./components/Testimony";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="principal-container">
-        <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        <Testimony {...DATA_INFO[0]} />
-        <Testimony {...DATA_INFO[1]} />
-        <Testimony {...DATA_INFO[2]} />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="principal-container">
+          <h1>Here is what our alumni say about freeCodeCamp:</h1>
+          <Testimony {...DATA_INFO[0]} />
+          <Testimony {...DATA_INFO[1]} />
+          <Testimony {...DATA_INFO[2]} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
