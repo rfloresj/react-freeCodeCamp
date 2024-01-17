1f18df87d523e6994fc15673c5aa9cc5438b81c0
rflores <104962825+rfloresj@users.noreply.github.com>
2024-01-17 12:02:43 -0600

    class-App-component-constructor-super()

--- 2-click-counter-class/src/App.js
+++ 2-click-counter-class/src/App.js
@@ -1,8 +1,45 @@
+import React from "react";
 import "./App.css";
 import freeCodeCampLogo from "./images/freecodecamp-logo.png";
 import Button from "./components/Button";
 import Counter from "./components/Counter";
 import { useState } from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numClicks: 0,
+    };
+  }
+
+  handleClick() {
+    this.setState();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="freecodecamp-logo-container">
+          <img
+            className="freecodecamp-logo"
+            src={freeCodeCampLogo}
+            alt="logo-freeCodeCamp"
+          />
+        </div>
+        <div className="main-container">
+          <Counter numClicks={this.state.numClicks} />
+          <Button text="Click" isButtonClick={true} handleClick={handleClick} />
+          <Button
+            text="Reset"
+            isButtonClick={false}
+            handleClick={restartCounter}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   const [numClicks, setNumClicks] = useState(0);
@@ -14,27 +51,6 @@ function App() {
   const restartCounter = () => {
     setNumClicks(0);
   };
-
-  return (
-    <div className="App">
-      <div className="freecodecamp-logo-container">
-        <img
-          className="freecodecamp-logo"
-          src={freeCodeCampLogo}
-          alt="logo-freeCodeCamp"
-        />
-      </div>
-      <div className="main-container">
-        <Counter numClicks={numClicks} />
-        <Button text="Click" isButtonClick={true} handleClick={handleClick} />
-        <Button
-          text="Reset"
-          isButtonClick={false}
-          handleClick={restartCounter}
-        />
-      </div>
-    </div>
-  );
 }
 
 export default App;
