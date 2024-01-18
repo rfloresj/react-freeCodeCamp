1f43eb1f69ce9d4046c6582d535b717a7e55dd16
rflores <104962825+rfloresj@users.noreply.github.com>
2024-01-18 10:59:53 -0600

    restartCounter-handleClick-setState-bind

--- 2-click-counter-class/src/App.js
+++ 2-click-counter-class/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import freeCodeCampLogo from "./images/freecodecamp-logo.png";
 import Button from "./components/Button";
 import Counter from "./components/Counter";
-import { useState } from "react";
 
 class App extends React.Component {
   constructor() {
@@ -11,10 +10,16 @@ class App extends React.Component {
     this.state = {
       numClicks: 0,
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.restartCounter = this.restartCounter.bind(this);
   }
 
   handleClick() {
-    this.setState();
+    this.setState(({ numClicks }) => ({ numClicks: numClicks + 1 }));
+  }
+
+  restartCounter() {
+    this.setState({ numClicks: 0 });
   }
 
   render() {
@@ -29,11 +34,15 @@ class App extends React.Component {
         </div>
         <div className="main-container">
           <Counter numClicks={this.state.numClicks} />
-          <Button text="Click" isButtonClick={true} handleClick={handleClick} />
+          <Button
+            text="Click"
+            isButtonClick={true}
+            handleClick={this.handleClick}
+          />
           <Button
             text="Reset"
             isButtonClick={false}
-            handleClick={restartCounter}
+            handleClick={this.restartCounter}
           />
         </div>
       </div>
@@ -41,7 +50,7 @@ class App extends React.Component {
   }
 }
 
-function App() {
+/* function App() {
   const [numClicks, setNumClicks] = useState(0);
 
   const handleClick = () => {
@@ -51,6 +60,6 @@ function App() {
   const restartCounter = () => {
     setNumClicks(0);
   };
-}
+} */
 
 export default App;
